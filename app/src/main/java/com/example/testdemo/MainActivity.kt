1d608e2decf8cb7fbe4f0b7d2153777962940a4e
scott2srikanth <srikanthreddy.com@gmail.com>
2021-05-10 19:56:08 +0530

    Sample Kotlin Android Demo

--- app/src/main/java/com/example/testdemo/MainActivity.kt
+++ app/src/main/java/com/example/testdemo/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.testdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txt);
+
+        var flag=false;
+        btn.setOnClickListener(View.OnClickListener {
+            // Code here executes on main thread after user presses button
+            if(flag){
+                txt.setText("Srikanth Reddy")
+                flag = false;
+            }
+            else {
+                txt.setText("Hello World")
+                flag = true;
+            }
+
+        })
+
     }
 }
