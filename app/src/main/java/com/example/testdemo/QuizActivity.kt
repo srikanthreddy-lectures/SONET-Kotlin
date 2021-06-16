6269705373d8c944fd7233fb8bf77b96e1bf03f5
scott2srikanth <srikanthreddy.com@gmail.com>
2021-06-16 20:34:38 +0530

    Quiz App Day 1

---
+++ app/src/main/java/com/example/testdemo/QuizActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
---
+++ app/src/main/java/com/example/testdemo/SonetQuiz.kt
@@ -0,0 +1,32 @@
+package com.example.testdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class SonetQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sonet_quiz)
+
+        var q_btn = findViewById<Button>(R.id.quiz_btn)
+        var q_name = findViewById<EditText>(R.id.quiz_name)
+
+        q_btn.setOnClickListener(View.OnClickListener {
+            Log.i("------->","Hello")
+            if(q_name.toString().isEmpty()){
+                Toast.makeText(this@SonetQuiz,"Please enter name",Toast.LENGTH_LONG).show()
+            }
+            else{
+                var next:Intent = Intent(this,QuizActivity()::class.java)
+                startActivity(next)
+                finish()
+            }
+        })
+    }
+}
