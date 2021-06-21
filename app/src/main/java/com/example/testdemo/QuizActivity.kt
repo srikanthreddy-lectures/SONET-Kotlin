98030a9b8113ae093ac7c10603f0a8b4cb494a16
scott2srikanth <srikanthreddy.com@gmail.com>
2021-06-21 20:42:30 +0530

    Quiz App Day 4(Final)

--- app/src/main/java/com/example/testdemo/QuizActivity.kt
+++ app/src/main/java/com/example/testdemo/QuizActivity.kt
@@ -1,10 +1,13 @@
 package com.example.testdemo
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
@@ -13,13 +16,14 @@ class QuizActivity : AppCompatActivity(),View.OnClickListener {
     private var listOfQuestions: ArrayList<Questions>? = null
     var currentPosition = 1
     lateinit var question:TextView
-    lateinit var optionOne:TextView
-    lateinit var optionTwo:TextView
-    lateinit var optionThree:TextView
-    lateinit var optionFour:TextView
+
     lateinit var submitBtn:Button
 
+    lateinit var optionList:List<TextView>
+
      var selectedChoice:Int=0
+
+    var score:Int=0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,70 +32,110 @@ class QuizActivity : AppCompatActivity(),View.OnClickListener {
         var utility = Utility()
         listOfQuestions = utility.getQuestion()
 
+       // listOfQuestions=utility.getQuestionFromGithub()
+
         var name:String? = intent.getStringExtra("name")
         var q_name = findViewById<TextView>(R.id.q_name_)
         q_name.text=name
 
         question = findViewById<TextView>(R.id.q_question)
-        optionOne = findViewById<TextView>(R.id.tv_option_one)
-        optionTwo = findViewById<TextView>(R.id.tv_option_two)
-        optionThree = findViewById<TextView>(R.id.tv_option_three)
-        optionFour = findViewById<TextView>(R.id.tv_option_four)
+
+        val optionOne = findViewById<TextView>(R.id.tv_option_one)
+        val optionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val optionThree = findViewById<TextView>(R.id.tv_option_three)
+        val optionFour = findViewById<TextView>(R.id.tv_option_four)
+
+        val progressBar = findViewById<ProgressBar>(R.id.q_progressBar)
+        progressBar.max=listOfQuestions!!.size
+        progressBar.progress=currentPosition
+
         submitBtn = findViewById<Button>(R.id.q_btn_next)
+
+        optionList = listOf<TextView>(optionOne,optionTwo,optionThree,optionFour)
 
 
         setQuestion();
 
-        optionOne.setOnClickListener(this)
-        optionTwo.setOnClickListener(this)
-        optionThree.setOnClickListener(this)
-        optionFour.setOnClickListener(this)
+        for (option in optionList){
+            option.setOnClickListener(this)
+        }
 
         submitBtn.setOnClickListener(View.OnClickListener {
             var ans = listOfQuestions!!.get(currentPosition-1).answer
 
-            checkAnswer(ans,selectedChoice);
+            val btnText:String = submitBtn.text.toString()
 
+            when(btnText){
+                "SUBMIT" ->{
+                    checkAnswer(ans,selectedChoice);
+
+                    for (option in optionList){
+                        option.setClickable(false)
+                    }
+
+                    if(listOfQuestions!!.size==currentPosition){
+                        submitBtn.setText("FINISH")
+                    }
+                    else{
+                        submitBtn.setText("NEXT")
+                    }
+
+                }
+                "NEXT" ->{
+                    currentPosition++
+                    setQuestion()
+                    for (option in optionList){
+                        option.setClickable(true)
+                    }
+                    progressBar.progress=currentPosition
+                }
+
+                "FINISH" ->{
+                    Toast.makeText(this,"Your score is $score",Toast.LENGTH_LONG).show()
+                    var intent = Intent(this,SonetQuiz()::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
         })
     }
 
-    private fun checkAnswer(ans:Int,id:Int){
-        when(ans){
+    private fun checkAnswer(ans:Int,selectedChoice:Int){
+        when(selectedChoice){
             1 ->{
-                Log.i("---->",id.toString())
-                if(ans ==id)
-                    optionOne.background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
-                else
-                    optionOne.background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(selectedChoice-1).background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(ans-1).background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
             }
             2 ->{
-                if(ans ==id)
-                    optionTwo.background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
-                else
-                    optionTwo.background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(selectedChoice-1).background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(ans-1).background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
             }
             3 ->{
-                if(ans ==id)
-                    optionThree.background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
-                else
-                    optionThree.background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(selectedChoice-1).background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(ans-1).background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
             }
             4 ->{
-                if(ans ==id)
-                    optionFour.background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
-                else
-                    optionFour.background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(selectedChoice-1).background=ContextCompat.getDrawable(this,R.drawable.wrong_option_color)
+                    optionList.get(ans-1).background=ContextCompat.getDrawable(this,R.drawable.right_option_color)
             }
+        }
+
+        if(ans==selectedChoice){
+            score+=1;
         }
     }
 
     fun setQuestion(){
         var questionObject = listOfQuestions!!.get(currentPosition-1)
         question.text= questionObject.question
-        optionOne.text= questionObject.optOne
-        optionTwo.text= questionObject.optTwo
-        optionThree.text= questionObject.optThree
-        optionFour.text= questionObject.optFour
+
+        optionList.get(0).text= questionObject.optOne
+        optionList.get(1).text= questionObject.optTwo
+        optionList.get(2).text= questionObject.optThree
+        optionList.get(3).text= questionObject.optFour
+
+        defaultOption();
+        submitBtn.setText("SUBMIT")
 
     }
 
@@ -99,16 +143,20 @@ class QuizActivity : AppCompatActivity(),View.OnClickListener {
         //logic
         when(v.id){
             R.id.tv_option_one ->{
-                selectedOption(optionOne,R.drawable.selected_option_border_bg)
+                selectedOption(optionList.get(0),R.drawable.selected_option_border_bg)
+                selectedChoice=1
             }
             R.id.tv_option_two ->{
-                selectedOption(optionTwo,R.drawable.selected_option_border_bg)
+                selectedOption(optionList.get(1),R.drawable.selected_option_border_bg)
+                selectedChoice=2
             }
             R.id.tv_option_three ->{
-                selectedOption(optionThree,R.drawable.selected_option_border_bg)
+                selectedOption(optionList.get(2),R.drawable.selected_option_border_bg)
+                selectedChoice=3
             }
             R.id.tv_option_four ->{
-                selectedOption(optionFour,R.drawable.selected_option_border_bg)
+                selectedOption(optionList.get(3),R.drawable.selected_option_border_bg)
+                selectedChoice=4
             }
         }
     }
@@ -120,9 +168,9 @@ class QuizActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     private fun defaultOption(){
-        optionOne.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
-        optionTwo.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
-        optionThree.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
-        optionFour.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        optionList.get(0).background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        optionList.get(1).background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        optionList.get(2).background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        optionList.get(3).background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
     }
 }
--- app/src/main/java/com/example/testdemo/Utility.kt
+++ app/src/main/java/com/example/testdemo/Utility.kt
@@ -1,15 +1,32 @@
 package com.example.testdemo
 
+import android.util.Log
+import java.net.HttpURLConnection
+import java.net.URL
+
 class Utility {
 
     fun getQuestion():ArrayList<Questions>{
         var list = ArrayList<Questions>()
         //logic
-        var q1 = Questions(1,"What is kotlin?","Scripting Language","Programming Language","Modern Language","Assembly Language",1)
+        var q1 = Questions(1,"What is kotlin?","Scripting Language","Programming Language","Modern Language","Assembly Language",3)
         var q2 = Questions(2,"What is Java?","Scripting Language","Programming Language","Modern Language","Assembly Language",2)
+        var q3 = Questions(3,"What is Browser?","Apple software","Google Tool","Desktop/Moibile Client","Language",3)
+
         list.add(q1);
         list.add(q2)
+        list.add(q3)
+        return list;
+    }
 
+    fun getQuestionFromGithub():ArrayList<Questions>{
+        var list = ArrayList<Questions>()
+        //logic
+        Thread(Runnable {
+            var response:String = URL("https://raw.githubusercontent.com/srikanthreddy-lectures/SONET-Kotlin/main/questions.json").readText()
+            Log.i("----> Response : ",response)
+
+        }).start()
         return list;
     }
 }
