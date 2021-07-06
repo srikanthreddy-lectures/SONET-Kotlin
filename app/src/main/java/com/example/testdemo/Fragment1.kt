b80a0b7e185617f1434c0af634027c1c395b14bd
scott2srikanth <srikanthreddy.com@gmail.com>
2021-07-06 19:59:42 +0530

    Fragments Day 2(Fragment --> Fragment)

--- app/src/main/java/com/example/testdemo/Fragment1.kt
+++ app/src/main/java/com/example/testdemo/Fragment1.kt
@@ -1,13 +1,29 @@
 package com.example.testdemo
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.testdemo.databinding.Fragment1Binding
 
-class Fragment1 : Fragment(R.layout.fragment1) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class Fragment1 : Fragment() {
+    //logic
+    lateinit var binder : Fragment1Binding
 
-        //logic
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binder = Fragment1Binding.inflate(layoutInflater)
+        return binder.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binder.fragmentChangeBtn.setOnClickListener(View.OnClickListener {
+            var fragmentManager = activity?.supportFragmentManager
+            var fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.demo_fragment_container, Fragment2())
+            fragmentTransaction?.commit()
+        })
+    }
 }
+
